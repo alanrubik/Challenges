559a9a0201dddedc64ed7d9a11ed1225be817a26
Roberto Valenzuela <valenzuelarober@gmail.com>
2021-03-19 23:42:09 -0600

    Create 108.C

---
+++ rob/108.C
@@ -0,0 +1,19 @@
+// 108. Convert Sorted Array to Binary Search Tree
+//
+// Given an integer array nums where the elements are sorted in ascending order,
+// convert it to a height-balanced binary search tree.
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+
+struct TreeNode* sortedArrayToBST(int* nums, int numsSize){
+
+}
+ 
