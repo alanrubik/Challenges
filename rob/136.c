aba6a0a15e7864acb4a0d7bf9fd90ed32a6ab63a
Roberto Valenzuela <valenzuelarober@gmail.com>
2021-03-25 23:04:53 -0600

    Update 136.c

--- rob/136.c
+++ rob/136.c
@@ -3,7 +3,66 @@
 // Given a non-empty array of integers nums, every element appears 
 // twice except for one. Find that single one.
 
-int singleNumber(int* nums, int numsSize){
 
+void deleteElement(int *nums, int numsSize, int elemPos);
+
+
+int singleNumber(int *nums, int numsSize)
+{
+    int unique;
+    int left = 0;
+    int right = left + 1;
+
+    for (;;)
+    {
+        if (nums[left] == nums[right])
+        {
+            deleteElement(nums, numsSize, right);
+            numsSize -= 1;
+            left++;
+
+            if (left + 1 < numsSize - 1)
+            {
+                right = left + 1;
+            }
+            else
+            {
+                unique = nums[left];
+                break;
+            }
+        }
+
+        else
+        {
+            if (right + 1 >= numsSize)
+            {
+                unique = nums[left];
+                break;
+            }
+            else
+            {
+                right++;
+            }
+        }
+    }
+
+    return (unique);
+}
+
+void deleteElement(int *nums, int numsSize, int elemPos)
+{
+    int i;
+
+    if (elemPos >= numsSize + 1)
+    {
+        printf("Deletion not possible\n");
+    }
+    else
+    {
+        for (i = elemPos; i < numsSize - 1; i++)
+        {
+            nums[i] = nums[i + 1];
+        }
+    }
 }
  
