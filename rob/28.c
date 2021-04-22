5ffffa64edc90b60609c0bac06d1a11768a7cec4
Roberto Valenzuela <valenzuelarober@gmail.com>
2021-04-22 13:03:13 -0500

    Update 28.c

--- rob/28.c
+++ rob/28.c
@@ -6,31 +6,21 @@
 
 int strStr(char * haystack, char * needle)
 {
-    int h_ind;
-    int n_ind;
-    int h_len;
-    int n_len;
-    int index;
-
-    h_len = strlen(haystack);
-    n_len = strlen(needle);
-
-    index = -1;
-
-    if(n_len > h_len) {
-        index = -1;
-    }
+    int hi, ni;
+    int hlen = strlen(haystack);
+    int nlen = strlen(needle);
     
-    if(n_len == 0) {
-        index = 0;
+    if (nlen > hlen)
+        return -1;
+    
+    if (nlen == 0)
+        return 0;
+    
+    for (hi = 0; hi < (hlen - nlen + 1); hi++) {
+        if (strncmp(&haystack[hi], needle, nlen) == 0)
+            return hi;
     }
 
-    for(h_ind = 0; h_ind < (h_len - n_len + 1); h_ind) {
-        if(strncmp(haystack, needle, n_len) == 0) {
-            index = h_ind;
-        }
-    }
-
-    return(index);
+    return -1;
 }
 
