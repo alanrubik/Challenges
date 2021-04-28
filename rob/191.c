ba0aa9be33d39ac7612f40aee3f7a065b7f322c4
Roberto Valenzuela <valenzuelarober@gmail.com>
2021-04-28 10:59:44 -0500

    Update 191.c

--- rob/191.c
+++ rob/191.c
@@ -4,9 +4,27 @@
 // bits it has (also known as the Hamming weight).
 
 
-int hammingWeight(uint32_t n)
+// 3 -> b'0011
+// 1 -> b'0001
+// &
+//   ->   ...1 = True
+
+#define NUMBER_BITS(A) (sizeof(A) * 8)
+
+int hammingWeight(uint32_t n) 
 {
-
-
+    
+    int count;
+    
+    count = 0;
+    
+    for(int i = 0; i < NUMBER_BITS(uint32_t); i++) {
+        if(n & 1) {
+            count++;            
+        }
+        n >>= 1;
+    }
+        
+    return count;
 }
-
+ 
