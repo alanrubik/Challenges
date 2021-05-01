4243f95e027da32774625063f2397191f4d53999
Roberto Valenzuela <valenzuelarober@gmail.com>
2021-04-30 22:45:01 -0500

    Update 190.c

--- rob/190.c
+++ rob/190.c
@@ -2,7 +2,18 @@
 //
 // Reverse bits of a given 32 bits unsigned integer.
 
+#define NUM_BITS(data_type) (sizeof(data_type) * 8) 
+
 uint32_t reverseBits(uint32_t n) {
     
+    uint32_t reversed_n = 0;
+    
+    for(int i = NUM_BITS(uint32_t) - 1; i >= 0; i--)
+    {
+        reversed_n = reversed_n + (n & 1) << i;  
+        n = n >> i;
+    }
+    
+    return reversed_n;
 }
  
